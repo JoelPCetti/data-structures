deaebba8e9d96bc484f96ce0f226f18ffa5c7239
JoelPCetti <joelpcetti@gmail.com>
2020-05-21 19:58:33 -0400

    Joel and Adam Data Structures --Stack, Queue, LinkedList

--- python/linked_list.py
+++ python/linked_list.py
@@ -1,19 +1,46 @@
 class LinkList:
   # write your __init__ method here that should store a 'head' value which the first Node in the LinkedList and a 'length' value which is the total number of Nodes in the LinkedList
+  def __init__(self, head = None):
+    self.head = head
+    self.next_node = None
 
   def add(self, data):
-    # write your code to ADD an element to the Linked List
-    pass
-
+    current = self.head
+    while current.next_node != None:
+      current = current.next_node
+    current.next_node = data
+  
   def remove(self, data):
-    # write your code to REMOVE an element from the Linked List
-    pass
+    if head.data == data:
+      head = head.next
+      return
+    current = self.head 
+    while current != None:
+      if current.next_node.data == data:
+        current.next_node = current.next_node.next_node
+        return
+      current = current.next_node
 
   def get(self, element_to_get):
-    # write you code to GET and return an element from the Linked List
-    pass
+    current = self.head
+    counter = 0
+    while current != None:
+      if current == element_to_get:
+        return counter
+      counter+= 1
 
 # ----- Node ------
 class Node:
-  # store your DATA and NEXT values here
-  pass
+  def __init__(self, data):
+    self.data = data
+    self.next_node = None
+
+
+
+        
+      
+    
+
+
+
+
